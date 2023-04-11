e35ee5a9f39dad96694d22ad0e45dede879910f6
yungsup <130447193+yungsup@users.noreply.github.com>
2023-04-11 22:09:21 +0900

    Update streamlit_app.py
    addi header and subheader messages to this

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')  
+
+streamlit.header('Breakfast Menu')
+
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+streamlit.subheader('Kale, Spinach & Rocket Smoothie')
+streamlit.subheader('Hard-Boiled Fee-Range Egg')
