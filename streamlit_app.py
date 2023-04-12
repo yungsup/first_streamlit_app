1ae243e4431ca1582d03203d8dbb48ac61ff0137
yungsup <130447193+yungsup@users.noreply.github.com>
2023-04-12 22:22:19 +0900

    Update streamlit_app.py
    added a question

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Fee-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
